52124307c5d7c5347bc00852bfc24f66dfc741d3
Anastasia <anastasiia.sukhanova@startupsoft.com>
2024-09-03 07:55:24 -0700

    Refactor: split on_build

--- exts/sim.info.display/sim/info/display/widget_info_manipulator.py
+++ exts/sim.info.display/sim/info/display/widget_info_manipulator.py
@@ -1,5 +1,4 @@
 from omni.ui import scene as sc
-from pxr import UsdGeom
 import omni.usd
 import omni.ui as ui
 from omni.ui import color as cl
@@ -10,11 +9,44 @@ class WidgetInfoManipulator(sc.Manipulator):
         super().__init__(**kwargs)
         self.usd_context = omni.usd.get_context()
         self.widgets = []
+        self._root = None
+        self.destroy()
+
+    def destroy(self):
+        self._root = None
+
+    @staticmethod
+    def on_build_widgets(prim):
+        custom_style = {"font_size": 25, "margin": 20}
+
+        with ui.ZStack():
+            ui.Rectangle(
+                style={
+                    "background_color": cl(0.2),
+                    "border_color": cl(0.7),
+                    "border_width": 2,
+                    "border_radius": 4,
+                }
+            )
+
+            with ui.VStack():
+                if prim.GetCustomData():
+                    formatted_data = "\n".join(
+                        f"{key}: {value}" for key, value in prim.GetCustomData().items()
+                    )
+                    ui.Label(
+                        formatted_data,
+                        height=0,
+                        alignment=ui.Alignment.LEFT,
+                        style=custom_style,
+                    )
+                else:
+                    ui.Label(
+                        "No meta found for this prim",
+                        style=custom_style,
+                    )
 
     def on_build(self):
-
-        for widget in self.widgets:
-            widget.frame.clear()
         self.widgets.clear()
 
         if not self.model:
@@ -30,48 +62,30 @@ class WidgetInfoManipulator(sc.Manipulator):
 
         all_prims = self.model.get_item("all_prims")
 
-        for prim_path in all_prims:
-            prim = stage.GetPrimAtPath(prim_path)
+        self._root = sc.Transform()
 
-            if prim.IsA(UsdGeom.Imageable):
+        with self._root:
+            for prim_path in all_prims:
+                prim = stage.GetPrimAtPath(prim_path)
+
                 position = self.model.get_position_for_prim(prim)
 
                 with sc.Transform(
                     transform=sc.Matrix44.get_translation_matrix(*position)
                 ):
                     with sc.Transform(scale_to=sc.Space.SCREEN):
-                        widget = sc.Widget(550, 150)
+                        widget = sc.Widget(
+                            500,
+                            150,
+                            update_policy=sc.Widget.UpdatePolicy.ON_MOUSE_HOVERED,
+                        )
+                        widget.frame.set_build_fn(
+                            lambda prim=prim: WidgetInfoManipulator.on_build_widgets(
+                                prim
+                            )
+                        )
                         self.widgets.append(widget)
-
-                        custom_style = {"font_size": 25, "margin": 20}
-                        with widget.frame:
-                            with ui.ZStack():
-                                ui.Rectangle(
-                                    style={
-                                        "background_color": cl(0.2),
-                                        "border_color": cl(0.7),
-                                        "border_width": 2,
-                                        "border_radius": 4,
-                                    }
-                                )
-                                if prim.IsA(UsdGeom.Imageable):
-                                    with ui.VStack():
-                                        if prim.GetCustomData():
-                                            formatted_data = "\n".join(
-                                                f"{key}: {value}"
-                                                for key, value in prim.GetCustomData().items()
-                                            )
-                                            ui.Label(
-                                                formatted_data,
-                                                height=0,
-                                                alignment=ui.Alignment.LEFT,
-                                                style=custom_style,
-                                            )
-                                        else:
-                                            ui.Label(
-                                                "No meta found for this prim",
-                                                style=custom_style,
-                                            )
 
     def on_model_updated(self, item):
         self.invalidate()
+        self.destroy()
