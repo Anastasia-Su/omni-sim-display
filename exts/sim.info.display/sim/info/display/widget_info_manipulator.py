56ef1e953719082a9b6fe124dc4519f3976ac5c1
Anastasia <anastasiia.sukhanova@startupsoft.com>
2024-09-03 04:29:11 -0700

    Add simultaneous info display

--- exts/sim.info.display/sim/info/display/widget_info_manipulator.py
+++ exts/sim.info.display/sim/info/display/widget_info_manipulator.py
@@ -1,82 +1,66 @@
 from omni.ui import scene as sc
-from omni.ui import color as cl
+from pxr import UsdGeom
+import omni.usd
 import omni.ui as ui
-from pxr import Gf
-
+from omni.ui import color as cl
 
 class WidgetInfoManipulator(sc.Manipulator):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        self._widget = None
-        self._root = None
-        self._name_label = None
-        self.destroy()
-
-    def destroy(self):
-        self._widget = None
-        self._root = None
-        self._name_label = None
-        
-        # self._slider_model = None
-
-    def on_build_widgets(self):
-        custom_style = {
-            "font_size": 25,
-            "margin": 20
-        }
-        with ui.ZStack():
-            ui.Rectangle(style={
-                "background_color": cl(0.2),
-                "border_color": cl(0.7),
-                "border_width": 2,
-                "border_radius": 4,
-            })
-
-            with ui.VStack():
-                self._name_label = ui.Label("", height=0, alignment=ui.Alignment.LEFT, style=custom_style)
-
-          
-        self.on_model_updated(None)
+        self.usd_context = omni.usd.get_context()
 
     def on_build(self):
-        self._root = sc.Transform(visible=False)
-        with self._root:
-            with sc.Transform(scale_to=sc.Space.SCREEN):
-                with sc.Transform(transform=sc.Matrix44.get_translation_matrix(0, 100, 0)):
-                    self._widget = sc.Widget(500, 150, update_policy=sc.Widget.UpdatePolicy.ON_MOUSE_HOVERED)
-                    self._widget.frame.set_build_fn(self.on_build_widgets)
-
-    def on_model_updated(self, _):
-        # if you don't have selection then show nothing
-        if not self.model or not self.model.get_item("name"):
-            if self._root:
-                self._root.visible = False
+        if not self.model:
             return
-        
-        if self._root:
-            self._root.visible = True
-        
+
+        if self.model.get_item("name") == "":
+            return
+
+        stage = self.model.usd_context.get_stage()
+
+        if not stage:
+            return
 
         all_prims = self.model.get_item("all_prims")
-        stage = self.model.usd_context.get_stage()
 
         for prim_path in all_prims:
             prim = stage.GetPrimAtPath(prim_path)
+            
+            if prim.IsA(UsdGeom.Imageable):
+                position = self.model.get_position_for_prim(prim)
 
-            position = self.model.get_position_for_prim(prim)
+                with sc.Transform(
+                    transform=sc.Matrix44.get_translation_matrix(*position)
+                ):
+                    with sc.Transform(scale_to=sc.Space.SCREEN):
+                        widget = sc.Widget(500, 150)  
+                        custom_style = {
+                            "font_size": 25,
+                            "margin": 20
+                        }
+                        with widget.frame:
+                            with ui.ZStack():
+                                ui.Rectangle(style={
+                                    "background_color": cl(0.2),
+                                    "border_color": cl(0.7),
+                                    "border_width": 2,
+                                    "border_radius": 4,
+                                })
+                                if prim.IsA(UsdGeom.Imageable):
+                                    with ui.VStack():
+                                        if prim.GetCustomData():
+                                            formatted_data = "\n".join(
+                                                f"{key}: {value}"
+                                                for key, value in prim.GetCustomData().items()
+                                            )
+                                            ui.Label(
+                                                formatted_data, 
+                                                height=0, 
+                                                alignment=ui.Alignment.LEFT, 
+                                                style=custom_style
+                                            )
+                                        else:
+                                            ui.Label("No meta found for this prim", style=custom_style)
 
-            if self._root:
-                self._root.transform = sc.Matrix44.get_translation_matrix(*position)
-                self._root.visible = True
-
-            if self._name_label:
-                if prim.GetCustomData():
-                    formatted_data = "\n".join(
-                        f"{key}: {value}"
-                        for key, value in prim.GetCustomData().items()
-                    )
-                    self._name_label.text = f"{formatted_data}"
-                else:
-                    self._name_label.text = "No meta found for this prim"
-
-            # self._name_label.text = f"Prim:{self.model.get_item('name')}"
+    def on_model_updated(self, item):
+        self.invalidate()
